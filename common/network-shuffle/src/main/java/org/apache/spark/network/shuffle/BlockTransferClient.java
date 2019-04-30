5925f308776e4392da6a1f80dc5f6e9e8547b8aa
“attilapiros” <piros.attila.zsolt@gmail.com>
2019-04-30 13:40:02 +0200

    doc fix.

--- common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockTransferClient.java
+++ common/network-shuffle/src/main/java/org/apache/spark/network/shuffle/BlockTransferClient.java
@@ -19,11 +19,7 @@
 public interface BlockTransferClient {
 
   /**
-   * Fetch a sequence of blocks from a remote node asynchronously,
-   *
-   * Note that this API takes a sequence so the implementation can batch requests, and does not
-   * return a future so the underlying implementation can invoke onBlockFetchSuccess as soon as
-   * the data of a block is fetched, rather than waiting for all blocks to be fetched.
+   * Fetch a sequence of blocks from a remote node asynchronously.
    */
   void fetchBlocks(
       String host,
